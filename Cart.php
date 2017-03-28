6c7d4698a03c7320b99ab2f53cf6a5a91ecf1250
Jesika19 <jesikabye.s@gmail.com>
2017-03-28 17:25:36 +0400

    Add files via upload

--- Cart.php
+++ Cart.php
@@ -57,9 +57,82 @@
         $this->removeProduct($product);
     }
   }
-     
+
+
   //Calculate total 
   public function calculateTotal(Product $product){
+    $productRef = $product->getProductRef();
+
+        
+      foreach($this->products as $product){ 
+
+          
+          if (array_key_exists('quantity',$product)) {
+            if (array_key_exists('item',$product)) {
+                //$productRef = $product[$productRef];                     
+                $quantity = $product['quantity'];
+                var_Dump($quantity);
+            
+                $price = $product['item']->getPrice();
+
+                var_Dump($price);
+                //if (isset($this->total['total'])) {
+
+
+                 //foreach($this->total as $value){
+                  //   if (in_array($productRef,$value)) {
+                 //$this->total[$productRef]['total'][] = $price * $quantity;
+                    
+                   
+                    // if(array_key_exists('total',$value)) {    
+                    //$this->total['total'][$productRef] = $price * $quantity;
+                    //$this->total['total'][$productRef][] = $price * $quantity;
+                       //if ($key){{
+                         //if (!isset($this->total[$productRef]['total'])){
+                              //if (!array_key_exists([$productRef] ['total'], $this->total)){
+                             
+                                  //$this->total[$productRef]['total'][] = $price * $quantity;
+                                  $this->total['total'][$productRef][] = $price * $quantity;
+                              
+                              //}
+                             //var_Dump( $this->total[$productRef]['total']);
+                              //}
+                       
+                        
+                         
+                      //  $key = array_search('total',$this->total);
+                
+                    
+
+                    //}
+                  
+                 //}
+                  // }
+                  
+                //}
+                
+              // $this->total['total'][] = $price * $quantity;
+                     
+            }       
+            
+          }
+
+  
+        
+      
+      } 
+  
+                                  
+  
+
+   
+    
+
+
+  }
+     
+  //Calculate total 
+ /* public function calculateTotal(Product $product){
     $productRef = $product->getProductRef();       
     foreach($this->products as $product){  
          
@@ -73,12 +146,23 @@
 
             var_Dump($price);
             //if (isset($this->total['total'])) {
+
+
+              // foreach($this->total as $value){
+              //   if (in_array($productRef,$value)) {
+                
+                
+                  //$this->total['total'][$productRef] = $price * $quantity;
+             //$this->total['total'][$productRef][] = $price * $quantity;
+             $this->total[$productRef]['total'][] = $price * $quantity;
+            
+              //   }
+              // }
                
-                $this->total['total'][$productRef] = $price * $quantity;
             //}
              
            // $this->total['total'][] = $price * $quantity;
-            
+          
                                
         }       
         
@@ -86,9 +170,9 @@
      
     } 
 
-         
 
-  }
+
+  }*/
 
 
   
